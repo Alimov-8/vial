9d69df070d5feb180a5257d393abeb8d8aa9b327
Alimov-8 <alimov.abdullokh8@gmail.com>
2026-09-23 22:14:38 +0500

    Add static routing with handlers

--- app.py
+++ app.py
@@ -2,13 +2,36 @@ from webob import Request, Response
 
 
 class Vial:
+    def __init__(self):
+        self.routes = dict()
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
 
     def handle_request(self, request):
-        user_agent = request.environ.get("HTTP_USER_AGENT", "No Agent Found")
         response = Response()
-        response.text = f"User Agen: {user_agent}"
+        handler = self.find_handler(request)
+        if handler is not None:
+            handler(request, response)
+            return response
+
+        self.default_response(response)
         return response
+
+    def find_handler(self, request):
+        for path, handler in self.routes.items():
+            if path == request.path:
+                return handler
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not Found."
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from app import Vial
 
+
 app = Vial()
 
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from Home page"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from About page"
